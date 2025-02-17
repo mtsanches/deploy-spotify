f8d05f3e6c46361e43d280033400386e78a001d0
Mauricio Sanches <mauriciotadeusanches@gmail.com>
2025-02-17 09:23:18 -0300

    fix: ajustes do process env

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import axios from "axios";
 
-const NODE_ENV = process.NODE_ENV;
-const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+//const NODE_ENV = process.NODE_ENV;
+//const URL = NODE_ENV === "development" ? "http://localhost:3001/api" : "/api";
+const URL = "https://deploy-spotify-id7s.onrender.com/api";
 
 const responseArtists = await axios.get(URL + "/artists");
 const responseSongs = await axios.get(URL + "/songs");
